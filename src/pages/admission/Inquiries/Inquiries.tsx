5ff9c7ced870524be5528298300854cdd978fc94
alok <alok09ax@gmail.com>
2025-08-13 11:14:14 +0530

    hello jee

--- src/pages/admission/Inquiries/Inquiries.tsx
+++ src/pages/admission/Inquiries/Inquiries.tsx
@@ -1,13 +1,14 @@
-import  { useEffect } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, RootState } from '../../../redux/store'; // adjust path if needed
+import { AppDispatch, RootState } from '../../../redux/store';
 import { getAllInQuiries } from '../../../redux/slices/admission';
 import InquiriesList from './InquiriesList';
 
-const Inquiries : React.FC = () => {
+const Inquiries: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
-
-  const { inquiries, loading, error } = useSelector((state: RootState) => state.admission);
+  const { inquiries, loading, error } = useSelector(
+    (state: RootState) => state.admission
+  );
 
   useEffect(() => {
     dispatch(getAllInQuiries());
@@ -15,12 +16,14 @@ const Inquiries : React.FC = () => {
 
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4 ">Admission Inquiries</h1>
+      <h1 className="text-2xl font-bold mb-4">Admission Inquiries</h1>
 
       {loading && <p>Loading inquiries...</p>}
       {error && <p className="text-red-500">Error: {error}</p>}
       {!loading && inquiries.length === 0 && <p>No inquiries found.</p>}
-      {!loading && inquiries.length > 0 && <InquiriesList inquiries={inquiries} />}
+      {!loading && inquiries.length > 0 && (
+        <InquiriesList inquiries={inquiries} />
+      )}
     </div>
   );
 };
